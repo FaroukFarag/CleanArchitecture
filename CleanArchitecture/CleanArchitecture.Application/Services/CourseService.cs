5206eb8f3b0b9fbeade38573edccd8cc3ce5c916
Farouk Farag <ffarag@proemcs.com>
2021-02-14 14:47:24 +0200

    Course Service Create Functionality

--- CleanArchitecture/CleanArchitecture.Application/Services/CourseService.cs
+++ CleanArchitecture/CleanArchitecture.Application/Services/CourseService.cs
@@ -1,5 +1,7 @@
 ﻿using CleanArchitecture.Application.Interfaces;
 using CleanArchitecture.Application.ViewModels;
+using CleanArchitecture.Domain.Commands;
+using CleanArchitecture.Domain.Core.Bus;
 using CleanArchitecture.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -10,10 +12,12 @@ namespace CleanArchitecture.Application.Services
     public class CourseService : ICourseService
     {
         private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
         }
 
         public CourseViewModel GetCourses()
@@ -22,6 +26,13 @@ namespace CleanArchitecture.Application.Services
             {
                 Courses = _courseRepository.GetCourses()
             };
+        }
+
+        public void Create(CourseViewModel model)
+        {
+            var createCourseCommand = new CreateCourseCommand(model.Name, model.Description, model.ImageUrl);
+
+            _bus.SendCommand(createCourseCommand);
         }
     }
 }
--- CleanArchitecture/CleanArchitecture.Application/ViewModels/CourseViewModel.cs
+++ CleanArchitecture/CleanArchitecture.Application/ViewModels/CourseViewModel.cs
@@ -7,6 +7,9 @@ namespace CleanArchitecture.Application.ViewModels
 {
     public class CourseViewModel
     {
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public string ImageUrl { get; protected set; }
         public IEnumerable<Course> Courses { get; set; }
     }
 }
